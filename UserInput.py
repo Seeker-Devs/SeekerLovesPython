bfcd1c0d4ac1b947d4b69b02bf8af6cdaa473a14
Seeker-Devs <asghargardezi72@gmail.com>
2020-07-04 00:03:16 -0700

    Update UserInput.py

--- UserInput.py
+++ UserInput.py
@@ -1,2 +1,35 @@
 #aight fams! Today, we're going to "study" about user input!
-input("Henlo senpai! Please input your name:")
+hello_world = input("Henlo senpai! Please input your name:")
+print(hello_world)
+if hello_world.lower() == "Seeker".lower(): #returns true, not case sensitive
+    print("True")
+else:
+    print("False")
+if hello_world.islower(): #CHECKS IF THE string is lower
+    print("ye, it's lower")
+else:
+    print("no")
+if hello_world is "Seeker": #== is used to check the equality between values and is is used to check if two values point to the same object.. or type... if we want to check the input
+    #we should use '==' operator... let's make use of 'is'!
+    print("ok")
+else:
+    print("no")
+age = input("Enter your age:")
+if age.isdigit(): #isalpha is for letter, checks if number is an integer
+    print("yessir")
+else:
+    print("dum")
+#is example
+a1 = "a"
+a2 = "a"
+if a1 is a2:
+    print("yes") #both are the same type.. strings
+else:
+    print("nooosoaosajdajs'")
+b1 = f"a + 2"
+b2 = f"a + 2"
+if b1 is b2:
+    print(" l o l")
+else:
+    print("ok")
+
