ef5ef37795a7c4b4d0c882d39d603b2ae01991c0
Seeker-Devs <49820135+Seeker-Devs@users.noreply.github.com>
2020-07-04 03:48:59 -0700

    Add files via upload

---
+++ SimpleCalculator.py
@@ -0,0 +1,39 @@
+print("Welcome to SimpleCalculator for this Python Tutorial by Seeker!")
+# -------- F U N C T I O N S -------- #
+def add(a, b):
+	return a + b
+def multiply(a, b):
+	return a * b
+def substract(a, b):
+	return a - b
+def divide(a, b):
+	return a / b
+def remainder(a, b):
+	return a % b
+# -------- I N P U T -------- #
+while True:
+	inputchoice = input("Enter: \nA to calculate \nB to multiply \nC to substract \nD to divide\n")
+	if inputchoice in ("A", "B", "C", "D"):
+		if inputchoice.lower() == "A".lower():
+			print("Loading...")
+			first = float(input("Enter your first number: "))
+			second = float(input("Enter your second number: "))
+			print(f"{first} + {second}\n=  {add(first, second)}")
+		elif inputchoice.lower() == "B".lower():
+			print("Loading")
+			first = float(input("Enter your first number: "))
+			second = float(input("Enter your second number: "))
+			print(f"{first} * {second}\n= {multiply(first, second)}")
+		elif inputchoice.lower() == "C".lower():
+			print("Loading")
+			first = float(input("Enter your first number: "))
+			second = float(input("Enter your second number: "))
+			print(f"{first} - {second}\n= {substract(first, second)}")
+		elif inputchoice.lower() == "D".lower():
+			print("Loading")
+			first = float(input("Enter your first number: "))
+			second = float(input("Enter your second number: "))
+			print(f"{first} ÷ {second}\n= {divide(first, second)}\nRemainder:  {remainder(first, second)}")
+		break	
+	else:
+		print("Invalid input.")
