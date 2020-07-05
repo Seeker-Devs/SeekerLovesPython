e65c7622b8127707a01e28f27f25987d7f0b5470
Seeker-Devs <asghargardezi72@gmail.com>
2020-07-05 03:26:16 -0700

    Fix class.py

--- Class.py
+++ Class.py
@@ -6,3 +6,32 @@
   \____| |_|  \__,_| |___/ |___/
                                 
 """
+class Car: #define the class
+    def honk(self): #you must pass self in function 'arguments'
+        print("Honk honk")
+    def carname(self, name="Test"): #name is a function argument too, just ignore self when using the functions
+        return name #returns the carname instead of printing it
+    def carspeed(self, speed):
+        if isinstance(speed, int):
+            return speed
+    def breaks(self):
+        return Car().carspeed(0)
+Car().honk() #access honk from class Car()
+input1 = input("Enter your car name:")
+nameofcar = Car().carname(input1)
+print("You car name is: {}".format(nameofcar))
+try:
+    input2 = int(input("Broom wroom! What speed would you like to drive the car at?"))
+    speedofcar = Car().carspeed(input2)
+    print(f"Your speed of car: {speedofcar}")
+except ValueError:
+    print("Speed must be an integer value.")
+    print("Car speed has been set to 20.")
+    Car().carspeed(20)
+input3 = input("Would you like to stop the car?")
+if input3.lower() == "Yes".lower():
+    speednow = Car().breaks()
+    print(f"Car speed now = {speednow}")
+else:
+    print("OH NOOOOOO! WE ALMOST CRASHED FRICK")
+    Car().breaks()
